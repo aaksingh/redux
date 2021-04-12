5282e036759783289db16f999b5f87e1c086c803
aakashaftershoot <aakash@aftershoot.co>
2021-04-12 20:44:43 +0530

    Multiple Actions

--- index.js
+++ index.js
@@ -2,18 +2,27 @@ import redux from "redux";
 import { createStore } from "redux";
 
 const BUY_CAKE = "BUY_CAKE";
-
+const BUY_ICECREAM = "BUY_ICECREAMS";
 const buyCake = {
   type: BUY_CAKE,
   info: "FIRST REDUX ACTION",
 };
 
+const buyIceCream = {
+  type: BUY_ICECREAM,
+  info: "SECOND REDUX ACTION",
+};
 const buyCakes = () => {
   return buyCake;
 };
 
+const buyIceCreams = () => {
+  return buyIceCream;
+};
+
 const initialState = {
   numofCakes: 10,
+  numofIceCreams: 10,
 };
 
 const reducer = (state = initialState, action) => {
@@ -22,6 +31,12 @@ const reducer = (state = initialState, action) => {
       return {
         ...state,
         numofCakes: state.numofCakes - 1,
+      };
+
+    case BUY_ICECREAM:
+      return {
+        ...state,
+        numofIceCreams: state.numofIceCreams - 1,
       };
     default:
       return state;
@@ -37,7 +52,11 @@ const unsubscribe = Store.subscribe(() => {
 });
 
 Store.dispatch(buyCakes());
+Store.dispatch(buyCakes());
+Store.dispatch(buyCakes());
+Store.dispatch(buyCakes());
+Store.dispatch(buyIceCreams());
+Store.dispatch(buyIceCreams());
+Store.dispatch(buyIceCreams());
 
-Store.dispatch(buyCakes());
-Store.dispatch(buyCakes());
 unsubscribe();
